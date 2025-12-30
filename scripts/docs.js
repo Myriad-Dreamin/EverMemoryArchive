a653bd92f7a6ba9b33f558766312a3709a36dbb5
Myriad-Dreamin <camiyoru@gmail.com>
2025-12-30 21:02:44 +0800

    fix: format

--- scripts/docs.js
+++ scripts/docs.js
@@ -5,7 +5,7 @@ import { globSync } from "fs";
  * Generate the documentation for the core and HTTP endpoints.
  */
 function docsGen() {
-  const entryFlag = it => `--entryPoints ${it}`;
+  const entryFlag = (it) => `--entryPoints ${it}`;
 
   const coreEntries = [
     "packages/ema/src/index.ts",
@@ -13,11 +13,13 @@ function docsGen() {
     "packages/ema/src/db/index.ts",
     "packages/ema/src/concept/index.ts",
     "packages/ema/src/skills/index.ts",
-  ].map(entryFlag)
+  ].map(entryFlag);
   execSync(
     `typedoc ${coreEntries.join(" ")} --tsconfig packages/ema/tsconfig.json --out docs/core`,
   );
-  const routes = globSync("packages/ema-ui/src/app/api/**/route.ts").map(entryFlag);
+  const routes = globSync("packages/ema-ui/src/app/api/**/route.ts").map(
+    entryFlag,
+  );
   execSync(
     `typedoc ${routes.join(" ")} --tsconfig packages/ema-ui/tsconfig.json --out docs/http`,
   );
