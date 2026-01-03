54247aa59e144bd68a4420ddfc7fe4a750e7b8c5
Myriad-Dreamin <camiyoru@gmail.com>
2026-01-03 21:14:06 +0800

    feat: gen ai example

---
+++ packages/ema/src/test-genai.ts
@@ -0,0 +1,84 @@
+import { GoogleGenAI } from "@google/genai";
+import * as fs from "node:fs";
+import type { Dispatcher, RequestInit, Response } from "undici";
+import { ProxyAgent, fetch } from "undici";
+
+/**
+ * A common fetch implementation that uses a proxy if HTTPS_PROXY or https_proxy is set.
+ */
+class FetchWithProxy {
+  private readonly dispatcher: Dispatcher | undefined;
+
+  constructor() {
+    // Uses proxy if HTTPS_PROXY or https_proxy is set
+    const https_proxy =
+      process.env.HTTPS_PROXY || process.env.https_proxy || "";
+    this.dispatcher = https_proxy ? new ProxyAgent(https_proxy) : undefined;
+  }
+
+  fetch(url: string, requestInit?: RequestInit) {
+    requestInit ??= {};
+    requestInit.dispatcher = this.dispatcher;
+    return fetch(url, requestInit);
+  }
+
+  createFetcher() {
+    return this.fetch.bind(this);
+  }
+}
+
+/**
+ * Reads the API key from the .env file.
+ */
+const apiKey = (() => {
+  // get from .env
+  const env = fs.readFileSync(".env", "utf-8");
+  const apiKey = env
+    .split("\n")
+    .find((line) => line.startsWith("GEMINI_API_KEY="))
+    ?.split("=")[1];
+  if (!apiKey) {
+    throw new Error("GEMINI_API_KEY is not set");
+  }
+  return apiKey;
+})();
+
+/**
+ * A wrapper around the GoogleGenAI class that uses a custom fetch implementation.
+ */
+class GenAI extends GoogleGenAI {
+  constructor(
+    apiKey: string,
+    private readonly fetcher: (
+      url: string,
+      requestInit?: RequestInit,
+    ) => Promise<Response>,
+  ) {
+    super({
+      apiKey,
+    });
+    if (!(this.apiClient as any).apiCall) {
+      throw new Error("apiCall cannot be patched");
+    }
+    // Monkey patches apiCall to use our fetch.
+    (this.apiClient as any).apiCall = async (url: string, requestInit: any) => {
+      return this.fetcher(url, requestInit).catch((e) => {
+        throw new Error(`exception ${e} sending request`);
+      });
+    };
+  }
+}
+
+/**
+ * The fetch should be optionally given by the classes who constructs `GenAI`
+ */
+const genAi = new GenAI(apiKey, new FetchWithProxy().createFetcher());
+
+// generate hello world
+
+const response = await genAi.models.generateContent({
+  model: "gemini-2.5-flash",
+  contents: ["Hello, world!"],
+});
+
+console.log(response.text);
